d350e363fab79d1b5a2655c59289289b2563194f
searide <112839232+searide@users.noreply.github.com>
2022-09-23 17:12:56 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["furima38450_BASIC_AUTH_USER"] && password == ENV["furima38450_BASIC_AUTH_PASSWORD"]
+    end
+  end
+
 end
