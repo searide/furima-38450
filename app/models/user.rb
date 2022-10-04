0ecb68acf2ccd5d436112c3c9eeda81ae4ef806d
searide <112839232+searide@users.noreply.github.com>
2022-10-04 15:11:54 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,12 @@ class User < ApplicationRecord
     validates :birthday
   end
 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers.'}
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Include both letters and numbers.'}
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Include both letters and numbers.'}
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Include both letters and numbers.'}
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Include both letters and numbers.'}
+  
   has_many :items
   has_many :purchases
 
