edf40d3370cdf63eaf67fd2b7e2bfba08ce317da
searide <112839232+searide@users.noreply.github.com>
2022-10-05 15:41:34 +0900

    アソシエーション修正
    現時点で不要なアソシエーションをコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
   validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters'}
   validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters'}
 
-  has_many :items
-  has_many :purchases
+  #has_many :items
+  #has_many :purchases
 
 end
