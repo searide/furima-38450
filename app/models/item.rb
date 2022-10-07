bea66a0189b934b7363e35dce7973b88d7ae6c2d
searide <112839232+searide@users.noreply.github.com>
2022-10-07 12:00:56 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -24,10 +24,12 @@ class Item < ApplicationRecord
   belongs_to :user
   # has_one :purchase
 
+  has_one_attached :image
+
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_cost
   belongs_to :prefecture
   belongs_to :delivery_date
-  
+
 end
