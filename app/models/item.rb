99a894a8dd3bb27de48daee9c1769f9f8b218047
searide <112839232+searide@users.noreply.github.com>
2022-10-07 11:43:37 +0900

    itemモデルの作成,バリデーション

---
+++ app/models/item.rb
@@ -0,0 +1,33 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  with_options presence: true do
+    validates :user_id
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :delivery_cost_id
+    validates :prefecture_id
+    validates :delivery_date_id
+    validates :price, numericality: { in: 300..9_999_999 }
+  end
+
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_cost_id
+    validates :prefecture_id
+    validates :delivery_date_id
+  end
+
+  belongs_to :user
+  # has_one :purchase
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_cost
+  belongs_to :prefecture
+  belongs_to :delivery_date
+  
+end
