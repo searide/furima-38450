deb5b7e427fac8bbd5cd2e6c4258f29f55df8841
searide <112839232+searide@users.noreply.github.com>
2022-10-07 17:30:25 +0900

    投稿機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
   def index
+    #@item = Item.all(created_at DESC)
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      @item = Item.new(item_params)
+      render :new
+    end
+  end
+
+private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :delivery_date_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
