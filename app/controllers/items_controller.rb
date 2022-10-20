cbd2840f325434b8613e81b922e51078113bee94
searide <112839232+searide@users.noreply.github.com>
2022-10-20 15:51:58 +0900

    itemモデル単体テストコード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new]
 
   def index
     #@item = Item.all(created_at DESC)
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Item, type: :model do
 
   before do
-    @item = FactoryBot.build(:item)
+    @item = FactoryBot.create(:item)
   end
 
   describe '商品出品' do
@@ -11,8 +11,13 @@ describe Item, type: :model do
       it '全ての項目が入力されていれば出品ができる' do
         expect(@item).to be_valid
       end
-    
+    end
     context '商品出品がうまくいかないとき' do
+      it 'ユーザー登録している人でないと出品できない' do
+        @item.user_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
       it 'imageが空では登録されない' do
         @item.image = nil
         @item.valid?
@@ -53,20 +58,30 @@ describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery date can't be blank")
       end
-      it 'それぞれのidで0が選択された場合は登録されない' do
+      it 'category_idで0が選択された場合は登録されない' do
         @item.category_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'condition_idで0が選択された場合は登録されない' do
         @item.condition_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it 'delivery_cost_idで0が選択された場合は登録されない' do
         @item.delivery_cost_id = '0' 
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery cost can't be blank")
+      end
+      it 'prefecture_idで0が選択された場合は登録されない' do
         @item.prefecture_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'delivery_date_idで0が選択された場合は登録されない' do
         @item.delivery_date_id = '0'
         @item.valid?
-        expect(@item.errors.full_messages).to include(
-          "Category can't be blank",
-          "Condition can't be blank",
-          "Delivery cost can't be blank",
-          "Prefecture can't be blank",
-          "Delivery date can't be blank"
-        )
+        expect(@item.errors.full_messages).to include("Delivery date can't be blank")
       end
       it 'priceが空では登録されない' do
         @item.price = ""
@@ -88,8 +103,6 @@ describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
-    end
-
     end
   end
 end
