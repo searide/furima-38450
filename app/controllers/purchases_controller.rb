d02d817f8b294af9ee5306a630263dd2a9510939
searide <112839232+searide@users.noreply.github.com>
2022-10-21 10:54:09 +0900

    商品購入機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,8 +2,33 @@ class PurchasesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @purchase_form = PurchaseForm.new
   end
 
   def create
+    @purchase_form = PurchaseForm.new(purchase_params)
+    if @purchase_form.valid?
+      pay_item
+      @purchase_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_b66d668a9c3f2ccc8a560b9c"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
